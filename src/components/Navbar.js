4e2345d9b1e9f5fa8c6d70c819a7594d4ae91aa8
bimaap <let.bimaap@gmail.com>
2022-08-04 13:35:10 +0700

    feat: card, sliceing home, navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,26 +4,28 @@ import profile from '../assets/images/profile.jpg'
 
 const Navbar = () => {
     return(
-        <div className="fash-navbar d-flex flex-fill align-items-center justify-content-between bc-primary">
-            <div className="c-secondary d-flex align-items-center">
-                <AiFillShopping className="fash-h2" />
-                <span className="fash-h2">Fashio</span>
-            </div>
-            <div className="d-flex gap-2">
-                <div className="fash-search bc-primary d-flex justify-content-between align-items-center">
-                    <input type="text" name="search" className="w-fill" placeholder="Search" />
-                    <AiOutlineSearch className="fash-h2 c-dark" />
+        <div className="fash-navbar bc-primary w-fill d-flex justify-content-center">
+            <div className="fash-control-navbar d-flex align-items-center justify-content-between h-fill">
+                <div className="c-secondary d-flex align-items-center">
+                    <AiFillShopping className="fash-h2" />
+                    <span className="fash-h2">Fashio</span>
                 </div>
-                <div className="fash-nav-filter d-flex align-items-center justify-content-center">
-                    <AiOutlineFilter className="fash-h2 c-dark" />
+                <div className="d-flex gap-2">
+                    <div className="fash-search bc-primary d-flex justify-content-between align-items-center">
+                        <input type="text" name="search" className="w-fill" placeholder="Search" />
+                        <AiOutlineSearch className="fash-h2 c-dark" />
+                    </div>
+                    <div className="fash-nav-filter d-flex align-items-center justify-content-center">
+                        <AiOutlineFilter className="fash-h2 c-dark" />
+                    </div>
                 </div>
-            </div>
-            <div className="d-flex gap-4 align-items-center">
-                <AiOutlineShoppingCart className="fash-h2 c-dark" />
-                <AiOutlineBell className="fash-h2 c-dark" />
-                <AiOutlineMail className="fash-h2 c-dark" />
-                <div className="fash-nav-profile">
-                    <img src={profile} alt='profile'/>
+                <div className="d-flex gap-4 align-items-center">
+                    <AiOutlineShoppingCart className="fash-h2 c-dark" />
+                    <AiOutlineBell className="fash-h2 c-dark" />
+                    <AiOutlineMail className="fash-h2 c-dark" />
+                    <div className="fash-nav-profile">
+                        <img src={profile} alt='profile'/>
+                    </div>
                 </div>
             </div>
         </div>
