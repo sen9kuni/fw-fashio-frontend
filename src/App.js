8393a268657ddf1f1ea707cdb64efa48dccb8412
muhammadridho <bakarudin947@gmail.com>
2022-08-04 12:34:27 +0700

    Slicing My Bag

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 import Home from './page/Home';
+import Bag from './page/Bag';
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />}></Route>
+      <Route path='/bag' element={<Bag />}></Route>
     </Routes>
   );
 }
