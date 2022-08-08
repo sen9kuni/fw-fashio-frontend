7de67d8aa48f102bd8e86cc5250d9b9a1330b271
bimaap <let.bimaap@gmail.com>
2022-08-08 22:54:29 +0700

    fix: link navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,19 @@
 
 import { AiFillShopping, AiOutlineSearch, AiOutlineFilter, AiOutlineShoppingCart, AiOutlineBell, AiOutlineMail } from "react-icons/ai";
 import profile from '../assets/images/profile.jpg'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+    const token = true
     return(
         <div className="fash-navbar bc-primary w-fill d-flex justify-content-center">
             <div className="fash-control-navbar d-flex align-items-center justify-content-between h-fill">
-                <div className="c-secondary d-flex align-items-center">
-                    <AiFillShopping className="fash-h2" />
-                    <span className="fash-h2">Fashio</span>
-                </div>
+                <Link className='text-decoration-none' to='/'>
+                    <div className="c-secondary d-flex align-items-center">
+                        <AiFillShopping className="fash-h2" />
+                        <span className="fash-h2">Fashio</span>
+                    </div>
+                </Link>
                 <div className="d-flex gap-2">
                     <div className="fash-search bc-primary d-flex justify-content-between align-items-center">
                         <input type="text" name="search" className="w-fill" placeholder="Search" />
@@ -20,12 +24,31 @@ const Navbar = () => {
                     </div>
                 </div>
                 <div className="d-flex gap-4 align-items-center">
-                    <AiOutlineShoppingCart className="fash-h2 c-dark" />
-                    <AiOutlineBell className="fash-h2 c-dark" />
-                    <AiOutlineMail className="fash-h2 c-dark" />
-                    <div className="fash-nav-profile">
-                        <img src={profile} alt='profile'/>
-                    </div>
+                    {token? 
+                        <div className="d-flex gap-4 align-items-center">
+                            <Link className='text-decoration-none' to='/bag'>
+                                <AiOutlineShoppingCart className="fash-h2 c-dark" />
+                            </Link>
+                            <AiOutlineBell className="fash-h2 c-dark" />
+                            <Link className='text-decoration-none' to='/chat'>
+                                <AiOutlineMail className="fash-h2 c-dark" />
+                            </Link>
+                            <Link className='text-decoration-none' to='/profile/seller/profile'>
+                                <div className="fash-nav-profile">
+                                    <img src={profile} alt='profile'/>
+                                </div>
+                            </Link>
+                        </div>
+                        :
+                        <div className="d-flex gap-4">
+                            <Link className='text-decoration-none' to='/login-customer'>
+                                <button className="fash-button bc-primary c-secondary" type='button'>Login</button>
+                            </Link>
+                            <Link className='text-decoration-none' to='/signup-customer'>
+                                <button className="fash-button bc-primary c-secondary" type='button'>Singup</button>
+                            </Link>
+                        </div>
+                    }
                 </div>
             </div>
         </div>
